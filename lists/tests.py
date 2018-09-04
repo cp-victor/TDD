ec25d445fbaa3b8c74ac131cb0144371013ef8fb
Victor Campos <victor.campos.depaula@gmail.com>
2018-09-04 11:36:57 -0300

    Pausa pro almocopython manage.py test functional_tests/

--- lists/tests.py
+++ lists/tests.py
@@ -17,7 +17,7 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_only_saves_items_when_necessary(self):
@@ -50,3 +50,6 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+
+class ListViewTest
