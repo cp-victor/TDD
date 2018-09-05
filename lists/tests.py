36d807706cf7a0992a5c3b4df56c1837edfd32c6
Victor Campos <victor.campos.depaula@gmail.com>
2018-09-05 11:19:15 -0300

    To perdido

--- lists/tests.py
+++ lists/tests.py
@@ -65,7 +65,8 @@ class ListAndItemModelTest(TestCase):
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request(self):
-        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
