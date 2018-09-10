ca41bd0e2f1c91ff89da29c018b1d35bf52cbf73
Victor Campos <victor.campos.depaula@gmail.com>
2018-09-10 08:25:49 -0300

    Split out unit tests into two files, and some corrections to models and functional_tests/base/test_list_validation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -19,12 +19,22 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.browser.refresh()
         self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
+    def wait_for_row_in_list_table(self, row_text)
         start_time = time.time()
         while True:
             try:
                 table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
+                rows = table.find_element_by_tag_name('tr')
                 self.assertIn(row_text, [row.text for row in rows])
                 return
             except (AssertionError, WebDriverException) as e:
